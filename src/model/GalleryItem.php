4ae3facc57897214be6524a34f9ac8fbcd17b3c5
Ishan Jayamanne <ishanjmails@gmail.com>
2018-10-16 19:38:57 +0000

    SS4 Upgrade

--- src/model/GalleryItem.php
+++ src/model/GalleryItem.php
@@ -1,11 +1,36 @@
 <?php
 
+namespace NZTA\Gallery\Model;
+
+use SilverStripe\AssetAdmin\Forms\UploadField;
+use SilverStripe\Assets\Image;
+use SilverStripe\Forms\FieldList;
+use SilverStripe\Forms\TextField;
+use SilverStripe\ORM\DataObject;
+use Page;
+
 /**
  * A GalleryItem consists an image with an attached caption that can
  * be used to create a Gallery of GalleryItems.
  */
 class GalleryItem extends DataObject
 {
+    /**
+     * @var string
+     */
+    private static $singular_name = 'Gallery item';
+
+    /**
+     * @var string
+     */
+    private static $plural_name = 'Gallery item';
+
+    /**
+     * @var string
+     */
+    private static $table_name = 'GalleryItem';
+
+
     /**
      * @var array
      */
@@ -18,8 +43,8 @@ class GalleryItem extends DataObject
      * @var array
      */
     private static $has_one = [
-        'Image' => 'Image',
-        'Page'  => 'Page'
+        'Image' => Image::class,
+        'Page'  => Page::class
     ];
 
     /**
@@ -49,11 +74,12 @@ class GalleryItem extends DataObject
         $fields->push(
             TextField::create('Caption', 'Caption')
                 ->setDescription(
-                    "(Optional) A short description of the image's content, limited to 35 characters. If not supplied, the image's title will"
-                    . " be used"
+                    "(Optional) A short description of the image's content, limited to 35 characters. 
+                    If not supplied, the image's title will be used"
                 )
                 ->setAttribute(
-                    'maxlength', 35
+                    'maxlength',
+                    35
                 )
         );
 
